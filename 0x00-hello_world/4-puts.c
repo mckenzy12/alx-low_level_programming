05d674c0d4bf69531d1e212d6be86c1a1da32a19
mckenzy12 <nwangwuudemezue@gmail.com>
2022-12-14 04:08:11 -0800

    Alx llp

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * Description: main - Puts the texts to stdout.
+ * Return: 0 should code runs successfully.
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
