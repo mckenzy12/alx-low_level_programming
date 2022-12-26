d195243955befc1978ce88e7246e8e58aa495899
mckenzy12 <mckenzy12>
2022-12-26 08:51:11 -0800

    bih

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ *
+ * @a: first num
+ *
+ * @b: second num
+ *
+ * Description: multiplies two integers.
+ *
+ * Return: multiplied result
+ */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return: 0-9 followed by newline
+ */
+void print_numbers(void)
+{
+int num = 0;
+
+while (num < 10)
+{
+_putchar(num + '0');
+num += 1;
+}
+
+_putchar('\n');
+}
